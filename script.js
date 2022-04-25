5838e06caff76852139e57eb52ca15853845c1ec
Shreyas Shastri <shreyasshastri22@gmail.com>
2022-04-25 23:48:44 +0530

    final changes

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let amount = document.getElementById("principal").value;
+    let rate = document.getElementById('range-text').innerText;
+    let y = document.getElementById("years");
+    let years = y.options[y.selectedIndex].value;
+    let interest = amount*parseFloat(rate)*parseInt(years) / 100;
+    const d = new Date();
+    let currentYear = d.getFullYear();
+    let finalYear = parseInt(currentYear) + parseInt(years);
+    let output = `If you deposit ${amount}, </br> at an interest rate of ${rate}&#37. </br>You will receive an amount of ${interest},</br>in the year ${finalYear}`;
+    document.getElementById("final-text").innerHTML = output;
 }
+
+function updateTextInput(val) {
+    document.getElementById('range-text').innerText=val; 
+  }
         
